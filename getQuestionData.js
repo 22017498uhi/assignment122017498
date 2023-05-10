59eaab97298325367c5acb58fc7baeb090d3882b
22017498uhi <113307467+22017498uhi@users.noreply.github.com>
2023-05-10 12:23:47 +0100

    GetQuestionData - added test for valid input string and invalid input string as well

--- getQuestionData.js
+++ getQuestionData.js
@@ -3,7 +3,7 @@ const data = require('./data');
 
 //this function takes question ID and fetches the relevant question data
 function getQuestionData(questionID){
-
+    return data[questionID];
 }
 
 module.exports = getQuestionData;
--- getQuestionData.test.js
+++ getQuestionData.test.js
@@ -3,16 +3,24 @@ const data = require('./data');
 
 const getQuestionData = require('./getQuestionData');
 
+test('getQuestionData response when valid string argument passed', () => {
+    expect(getQuestionData('balances')).toHaveProperty('balances'); //valid response will have same property as function input
+})
+
+test('getQuestionData response when data not found for string argument passed', () => {
+    expect(getQuestionData('abcd')).toBeFalsy(); //most likely empty object will be returned
+})
+
 test('getQuestionData response when no argument passed', () => {
     expect(getQuestionData()).toBe('questionID must be supplied.');
 })
 
 test('getQuestionData response when number argument passed', () => {
-    expect(getQuestionData()).toBe('questionID must be a string.');
+    expect(getQuestionData(123)).toBe('questionID must be a string.');
 })
 
 test('getQuestionData response when null argument passed', () => {
-    expect(getQuestionData()).toBe('questionID must be a string.');
+    expect(getQuestionData(null)).toBe('questionID must be a string.');
 })
 
 
