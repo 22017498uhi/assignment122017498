59e4b29a2469616cdcb5c299c0318da557e76b0d
22017498uhi <113307467+22017498uhi@users.noreply.github.com>
2023-05-10 12:35:45 +0100

    GetQuestionData - function argument related tests clubbed under describe block

--- getQuestionData.test.js
+++ getQuestionData.test.js
@@ -3,24 +3,26 @@ const data = require('./data');
 
 const getQuestionData = require('./getQuestionData');
 
-test('getQuestionData response when valid string argument passed', () => {
-    expect(getQuestionData('balances')).toHaveProperty('balances'); //valid response will have same property as function input
+describe('GetQuestionData behaviour based on various input arguments', () => {
+
+    test('when valid string argument passed', () => {
+        expect(getQuestionData('balances')).toHaveProperty('questions'); //valid response will have questions property atleast
+    })
+
+    test('when data not found for string argument passed', () => {
+        expect(getQuestionData('abcd')).toBeFalsy(); //most likely empty object or undefined will be returned
+    })
+
+    test('when no argument passed', () => {
+        expect(getQuestionData()).toBe('questionID must be supplied.');
+    })
+
+    test('when number argument passed', () => {
+        expect(getQuestionData(123)).toBe('questionID must be a string.');
+    })
+
+    test('when null argument passed', () => {
+        expect(getQuestionData(null)).toBe('questionID must be a string.');
+    })
+
 })
-
-test('getQuestionData response when data not found for string argument passed', () => {
-    expect(getQuestionData('abcd')).toBeFalsy(); //most likely empty object will be returned
-})
-
-test('getQuestionData response when no argument passed', () => {
-    expect(getQuestionData()).toBe('questionID must be supplied.');
-})
-
-test('getQuestionData response when number argument passed', () => {
-    expect(getQuestionData(123)).toBe('questionID must be a string.');
-})
-
-test('getQuestionData response when null argument passed', () => {
-    expect(getQuestionData(null)).toBe('questionID must be a string.');
-})
-
-
