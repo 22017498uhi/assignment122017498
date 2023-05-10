70f96470997e2c1ca5da9f0b395eb58d2323a461
22017498uhi <113307467+22017498uhi@users.noreply.github.com>
2023-05-10 16:45:39 +0100

    GetQuestionData : added tests for response data

--- getQuestionData.test.js
+++ getQuestionData.test.js
@@ -3,7 +3,8 @@ const data = require('./data');
 
 const getQuestionData = require('./getQuestionData');
 
-describe('GetQuestionData behaviour based on various input arguments', () => {
+//Write tests related to function argument
+describe('GetQuestionData : behaviour based on various input arguments', () => {
 
     test('when valid string argument passed', () => {
         expect(getQuestionData('balances')).toHaveProperty('questions'); //valid response will have questions property atleast
@@ -23,6 +24,42 @@ describe('GetQuestionData behaviour based on various input arguments', () => {
 
     test('when null argument passed', () => {
         expect(getQuestionData(null)).toBe('questionID must be supplied.');
+    })
+
+})
+
+//Write tests related to response
+describe('GetQuestionData : response data', () => {
+
+    test('when response contains questions property as expected', () => {
+        expect(getQuestionData('balances')).toHaveProperty('questions');
+    })
+
+    test('when response contains hints property as expected', () => {
+        expect(getQuestionData('balances')).toHaveProperty('hint');
+    })
+
+
+    test('when response contains answer property', () => {
+        const resp = getQuestionData('balances');
+
+        expect(resp).toHaveProperty('questions.fullquestion.answer');
+
+
+    })
+
+    //if answer has 0 or 1 choices then it doesnt make sense. user must select out of 2 options at bare minimum
+    test('when response contains answer property which should be an array with atleast 2 elements', () => {
+        const resp = getQuestionData('balances');
+
+        expect(resp).toHaveProperty('questions.fullquestion.answer');
+
+        const answerData = resp.questions.fullquestion.answer;
+
+        expect(answerData).not.toHaveLength(0);
+        expect(answerData).not.toHaveLength(1);
+
+
     })
 
 })
